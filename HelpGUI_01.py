f5ee84f720a8a28d01e287f6ec3f4ef955442722
Marea <mtteoiaki@gmail.com>
2020-04-22 08:21:25 +1200

    Made help box

--- HelpGUI_01.py
+++ HelpGUI_01.py
@@ -28,6 +28,41 @@ class Converter:#"Foo" and "bar" are just silly names  popularised by MIT about 
         print("You asked for help!")
         get_help = Help(self)
         get_help.help_text.configure(text="Help text goes here!")
+class Help:
+    def __init__(self, partner):
+         
+        background="orange"
+        
+        #disable help button
+        partner.help_button.config(state=DISABLED)
+        
+        #Sets up child window (ie: help box)
+        self.help_box = Toplevel()
+        
+        #If users press x button for the help box, you cant access it again so this fixes it
+        self.help_box.protocol('WM_DELETE_WINDOW', partial(self.close_help, partner))
+        #Set up GUI Frame
+        self.help_frame = Frame(self.help_box, width=300, bg=background)
+        self.help_frame.grid()
+        
+        #Set up Help heading (Row 0)
+        self.how_heading = Label(self.help_frame, text="Help / Instructions", 
+                                 font="arial 14 bold", bg=background)
+        self.how_heading.grid(row=0)
+        #Help Text (label, row 1)
+        self.help_text = Label(self.help_frame, text="",
+                               justify=LEFT, width=40, bg=background, wrap=250)
+        self.help_text.grid(row=1)
+        
+        #Dismiss button (row 2)
+        self.dismiss_btn = Button(self.help_frame, text="Dismiss", width=10, bg="white",
+                                  command=partial(self.close_help, partner))
+        self.dismiss_btn.grid(row=2, pady=10)
+    
+    def close_help(self, partner):
+        #Put help button to normal...
+        partner.help_button.config(state=NORMAL)
+        self.help_box.destroy()
 if __name__ == "__main__":
     root =Tk()# this means when we run root we will create a Tk window
     root.title( "Temperature Converter")
