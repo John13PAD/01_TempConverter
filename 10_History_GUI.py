4d18b84bf40b183cb026bcca1abb7f9e4da34724
John13PAD <63984003+John13PAD@users.noreply.github.com>
2020-05-28 09:43:33 +1200

    History_v1(Done at school)

---
+++ 10_History_GUI.py
@@ -0,0 +1,96 @@
+from tkinter import * #import all of tkinter tools
+from functools import partial #This prevents unwanted windows
+import random
+class Converter:#"Foo" and "bar" are just silly names  popularised by MIT about 1969
+    def __init__(self, parent):
+        #This is formatting variables
+        background_color = "light blue"
+        
+        #in actual program this is blank and is populated with user calculations
+        self.all_calc_list = ['0 degree C is -17.8 degrees F',
+        '0 degrees C is 32 degrees F',
+        '40 degrees C is 104 degrees F',
+        '40 degrees C is 4.4 degrees F',
+        '12 degrees C is 53.6 degrees F',
+        '24 degrees C is 75.2 degrees F',
+        '100 degrees C is 37.8 degrees F']
+        #Continue from 15
+        
+        #Convert main screen GUI
+        self.converter_frame = Frame(width=600, height=600, bg= background_color)
+        self.converter_frame.grid()
+        
+        #Temperature Conversion Heading (row 0)
+        self.temp_converter_label = Label(self.converter_frame, text="Temperature Converter",
+                                          font=("Arial", "32", "bold"),
+                                          bg= background_color,
+                                          padx=10, pady=10)
+        self.temp_converter_label.grid(row=0)
+        
+        self.history_button = Button(self.converter_frame, text="History",
+                                  font=("Arial","14"),
+                                  padx=10, pady=10, command=self.history)
+        self.history_button.grid(row=1)
+        
+    def history(self):
+        print("You asked for history!")
+        get_history = history(self)
+        get_history.history_text.configure(text="History text goes here!")
+class history:
+    def __init__(self, partner):
+
+        background="#a9ef99" #pale green
+
+        #disable history button
+        partner.history_button.config(state=DISABLED)
+
+        #Sets up child window (ie: history box)
+        self.history_box = Toplevel()
+
+        #If users press x button for the history box, you cant access it again so this fixes it
+        self.history_box.protocol('WM_DELETE_WINDOW', partial(self.close_history, partner))
+        #Set up GUI Frame
+        self.history_frame = Frame(self.history_box, width=300, bg=background)
+        self.history_frame.grid()
+
+        #Set up history heading (Row 0)
+        self.how_heading = Label(self.history_frame, text="Calcuation History", 
+                                 font="arial 14 bold", bg=background)
+        self.how_heading.grid(row=0)
+        #history Text (label, row 1)
+        self.history_text = Label(self.history_frame, 
+                                  text="Here are your most recent "
+                                       "calcuations. Please use the"
+                                       "export button to create a text "
+                                       "file of all your calculations for" 
+                                       "this session", wrap=250,
+                                       font="arial 10 italic",
+                                       justify=LEFT, width=40, bg=background, )
+        self.history_text.grid(row=1)
+        
+        #History output goes here
+        self.history_text = Label(self.history_frame, 
+                                  text="Here are your most recent "
+                                       "calcuations. Please use the"
+                                       "export button to create a text "
+                                       "file of all your calculations for" 
+                                       "this session", wrap=250,
+                                       font="arial 10 italic",
+                                       justify=LEFT, width=40, bg=background, )        
+        ##Dismiss button (row 2)
+        #self.dismiss_btn = Button(self.history_frame, text="Dismiss", width=10, bg="white",
+                                  #command=partial(self.close_history, partner))
+        #self.dismiss_btn.grid(row=2, pady=10)
+
+    def close_history(self, partner):
+        #Put history button to normal...
+        partner.history_button.config(state=NORMAL)
+        self.history_box.destroy()
+if __name__ == "__main__":
+    root =Tk()# this means when we run root we will create a Tk window
+    root.title( "Temperature Converter")
+    # this line puts a header in the window 'paren' that we are opening
+    something = Converter(root)
+    #this line places the class Foo into the root window
+    root.mainloop()
+    #this run the application mainloop in root window
