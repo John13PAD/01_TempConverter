21dfeb6889dd457ff4119a4217fea0839b00139e
John13PAD <63984003+John13PAD@users.noreply.github.com>
2020-06-09 12:35:34 +1200

    Finished Episode 18

---
+++ 11_Export_GUI_v0.py
@@ -0,0 +1,102 @@
+from tkinter import * #import all of tkinter tools
+from functools import partial #This prevents unwanted windows
+
+import random
+
+class Converter:#"Foo" and "bar" are just silly names  popularised by MIT about 1969
+    def __init__(self, parent):
+        #This is formatting variables
+        background_color = "light blue"
+        
+        #Convert main screen GUI
+        self.converter_frame = Frame(width=300, height=300, bg= background_color, pady=10)
+        self.converter_frame.grid()
+        
+        #Temperature Conversion Heading (row 0)
+        self.temp_converter_label = Label(self.converter_frame, text="Temperature Converter",
+                                          font=("Arial", "16", "bold"),
+                                          bg= background_color,
+                                          padx=10, pady=10)
+        self.temp_converter_label.grid(row=0)
+        
+        self.export_button = Button(self.converter_frame, text="Export",
+                                  font=("Arial","14"),
+                                  padx=10, pady=10, command=self.export)
+        self.export_button.grid(row=1)
+        
+    def export(self):
+        get_export = Export(self)
+
+class Export:
+    def __init__(self, partner):
+         
+        background="#a9ef99"
+        
+        #disable export button
+        partner.export_button.config(state=DISABLED)
+        
+        #Sets up child window (ie: export box)
+        self.export_box = Toplevel()
+        
+        #If users press x button for the export box, you cant access it again so this fixes it
+        self.export_box.protocol('WM_DELETE_WINDOW', partial(self.close_export, partner))
+        #Set up GUI Frame
+        self.export_frame = Frame(self.export_box, width=300, bg=background)
+        self.export_frame.grid()
+        
+        #Set up Export heading (Row 0)
+        self.how_heading = Label(self.export_frame, text="Export / Instructions", 
+                                 font="arial 14 bold", bg=background)
+        self.how_heading.grid(row=0)
+        #Export Text (label, row 1)
+        self.export_text = Label(self.export_frame, text="Enter a filename "
+                                                                                                    "in the box beow"
+                                                                                                    "and press Save"
+                                                                                                    "button to save your"
+                                                                                                    "your caculation history"
+                                                                                                    "to a text file",
+                               justify=LEFT, width=40, bg=background, wrap=250)
+        self.export_text.grid(row=1)
+        
+        #Warning text (Label, row 2)
+        self.export_text = Label(sef.export_frame, text="if the filename"
+                                                                                                    "you enter below"
+                                                                                                    "already exists,"
+                                                                                                    "its contents will"
+                                                                                                    "be replaced with"
+                                                                                                    "your calculation"
+                                                                                                    "history",
+                         justify = LEFT, bg="#ffafaf", fg="maroon",
+                                  font="Arial 10 italic", wrap=225, padx=10, pady=10)
+        self.export_text.grid(row=2, pady=10)
+
+        #filename entry box (row 3)
+        self.filename_entry = Entry(self.export_frame, width=20,
+                                                           font="Arial 14 bold", justify=CENTER)
+        self.filename_entry.grid(row=3, pady=10)
+        
+        #Save / Cancel Frame (row 4)
+        self.save_cancel_frame = Frame(self.export_frame)
+        self.save_cancel_frame.grid(row=5, pady=10)
+        
+        #Save and cancel buttons (row 0 of save_cancel_frame)
+        self.save_button = Button(self.save_cancel_frame, text="Save")
+        self.save_button.grid(row=0, column=0)
+        
+        self.cancel_button = Button(self.save_cancel_frame, text="Cancel",
+                                                               command=partial(self.close_export, partner))
+        sef.cancel_button.grid(row=0, column=1)
+        
+    
+    def close_export(self, partner):
+        #Put export button to normal...
+        partner.export_button.config(state=NORMAL)
+        self.export_box.destroy()
+if __name__ == "__main__":
+    root =Tk()# this means when we run root we will create a Tk window
+    root.title( "Temperature Converter")
+    # this line puts a header in the window 'paren' that we are opening
+    something = Converter(root)
+    #this line places the class Foo into the root window
+    root.mainloop()
+    #this run the application mainloop in root window
